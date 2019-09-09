259af4ca7de63ecf9cef345920cc9208a57f659a
Luis Ferreira <hidnasio@gmail.com>
2019-09-08 21:35:19 -0300

    Add ejercicio 3

--- ejercicio_1_code/ejercicio_1_code.ino
+++ ejercicio_1_code/ejercicio_1_code.ino
@@ -1,14 +1,9 @@
 #include <LiquidCrystal.h>
-#include <DHT.h>
 
-#define LCD_RS 2
-#define LCD_E 4
-#define LCD_D4 8
-#define LCD_D5 9
-#define LCD_D6 10
-#define LCD_D7 11
-#define LCD_ROWS 2
-#define LCD_COLS 16
+const byte LCD_RS = 2, LCD_E = 4, LCD_D4 = 8, LCD_D5 = 9,
+           LCD_D6 = 10, LCD_D7 = 11, LCD_ROWS = 2, LCD_COLS = 16;
+
+long number = 0;
  
 LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
@@ -18,5 +13,11 @@ void setup() {
   lcd.print("Hello World!");
 }
  
-void loop() {  
+void loop() {
+  number = number + 1;
+
+  lcd.setCursor(0, 1);
+  lcd.print(number);
+
+  delay(1000);
 }
--- ejercicio_3_code/ejercicio_3_code.ino
+++ ejercicio_3_code/ejercicio_3_code.ino
@@ -10,20 +10,31 @@ const byte LCD_RS = 2, LCD_E = 4, LCD_D4 = 8, LCD_D5 = 9,
 LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 DHT dht(DHTPIN, DHTTYPE);
 
+#define TOGGLE_TEMP_TIME 10000
+long lastTime;
+boolean useFahrenheit = false;
+
 void setup() {
   lcd.begin(LCD_COLS, LCD_ROWS);
   lcd.setCursor(0, 0);
   lcd.print("Initializing...");
   dht.begin();
+  lastTime = millis();
 }
- 
-void loop() { 
-  delay(5000);
+
+void loop() {
+  delay(2000);
+
+  if ((millis() - lastTime) > TOGGLE_TEMP_TIME) {
+    lastTime = millis();
+    useFahrenheit = !useFahrenheit;
+  }
+
   float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float t = dht.readTemperature(useFahrenheit);
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Temp (*C): ");
+  lcd.print(useFahrenheit ? "Temp (*F): " : "Temp (*C): ");
   lcd.print(t);
   lcd.setCursor(0, 1);
   lcd.print("Hum: ");
